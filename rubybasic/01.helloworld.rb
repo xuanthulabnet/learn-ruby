1a99f3e6b4a98daf7bcf3654db5a77a97d309f7d
XuanThuLab <xuanthulab.net@gmail.com>
2020-01-21 01:04:59 +0700

    Up

--- rubybasic/01.helloworld.rb
+++ rubybasic/01.helloworld.rb
@@ -1,1 +1,9 @@
-puts 
+=begin
+    Đây là đoạn code Ruby đầu tiên
+    Đoạn comment này viết trên nhiều dòng
+=end
+
+puts "Hello world!"
+
+# Có thể dùng lệnh print "chuỗi" để in ra chuỗi
+# không có xuống dòng sau khi in ra
